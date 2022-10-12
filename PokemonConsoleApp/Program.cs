d59dea821d510d89e1f6490ddda3bf9ba8d260b7
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-10-12 14:21:18 -0500

    Hello World

---
+++ PokemonConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string name = "charizard";
+var ApiString = $"https://pokeapi.co/api/v2/pokemon/{name}/";
